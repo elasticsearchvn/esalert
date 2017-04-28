2fed29bac12c89155e5492d1751472a1f892f81e
Anh Le <anhlqn@gmail.com>
2017-04-27 21:45:38 -0700

    Clean up unnecessary variable

--- esalert.ps1
+++ esalert.ps1
@@ -1,7 +1,4 @@
-# Variables contain script attributes
-$scriptDir = Split-Path -parent $MyInvocation.MyCommand.Definition
-
-Get-ChildItem -Path "$scriptDir\rules\*.ps1" | foreach {Start-Process Powershell.exe -ArgumentList "-file $($_.FullName)" -WindowStyle Hidden}
+Get-ChildItem -Path "$PSScriptRoot\rules\*.ps1" | foreach {Start-Process Powershell.exe -ArgumentList "-file $($_.FullName)" -WindowStyle Hidden}
 
 # Replace above for testing purpose
-# Get-ChildItem -Path "$scriptDir\rules\*.ps1" | foreach {& $_.FullName}
+# Get-ChildItem -Path "$PSScriptRoot\rules\*.ps1" | foreach {& $_.FullName}
